b90ac43a4215579d0cf5ac2d2399a901a6e04a7b
waffleprinter <jccpainchaud@gmail.com>
2024-11-19 17:02:35 -0500

    Delete Main class

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Money money1 = new Money(1.09124214);
-        Money money2 = new Money(2.0041241);
-        System.out.println(money1.add(money2));
-    }
-}
